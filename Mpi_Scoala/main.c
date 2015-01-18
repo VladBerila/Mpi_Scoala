e9e913fb6c57cd2f8bcd028a28ea3245f7a0d9ff
Vlad <vlad.berila@gmail.com>
2015-01-18 17:41:01 +0200

    add

---
+++ Mpi_Scoala/main.c
@@ -0,0 +1,172 @@
+//
+//  main.c
+//  MPIDEMO
+//
+//  Created by Vlad Berila on 09/10/14.
+//  Copyright (c) 2014 ___VLADBERILA___. All rights reserved.
+//
+
+#include <stdio.h>
+#include <mpi.h>
+#include <string.h>
+#include <stdlib.h>
+
+//Unde poate fi
+struct Pozitie
+{
+    //N,E,S,V
+    char directie;
+    int i,j;
+    int iAnterior, jAnterior;
+};
+
+struct Vedere
+{
+    //T,C,O,R
+    char fata, spate, stanga, dreapta;
+};
+
+struct Profesor
+{
+    int i,j;
+    char directie;
+} profesor;
+
+int Harta[100][100];
+int N,M;
+
+void master();
+void slave();
+
+int main(int argc, char * argv[])
+{
+    
+    // nr de procese
+    int nNumOfProcs, rank;
+    
+    // insert code here...
+    MPI_Init(&argc , &argv);
+    
+    MPI_Comm_size(MPI_COMM_WORLD, &nNumOfProcs);
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    if(rank == 0)
+        master();
+    else
+        slave();
+    
+    MPI_Finalize();
+    
+    return 0;
+}
+
+struct Vedere getVedere()
+{
+    struct Vedere vedere;
+    int i = profesor.i;
+    int j = profesor.j;
+    
+    if(profesor.directie == 'N')
+    {
+        vedere.fata = Harta[i-1][j];
+        vedere.dreapta = Harta[i][j+1];
+        vedere.spate = Harta[i+1][j];
+        vedere.stanga = Harta[i][j-1];
+    }
+    
+    if(profesor.directie == 'S')
+    {
+        vedere.fata = Harta[i+1][j];
+        vedere.dreapta = Harta[i][j-1];
+        vedere.spate = Harta[i-1][j];
+        vedere.stanga = Harta[i][j+1];
+    }
+    
+    if(profesor.directie == 'E')
+    {
+        vedere.fata = Harta[i][j+1];
+        vedere.dreapta = Harta[i+1][j];
+        vedere.spate = Harta[i][j-1];
+        vedere.stanga = Harta[i-1][j];
+    }
+    
+    if(profesor.directie == 'V')
+    {
+        vedere.fata = Harta[i][j-1];
+        vedere.dreapta = Harta[i-1][j];
+        vedere.spate = Harta[i][j+1];
+        vedere.stanga = Harta[i+1][j];
+    }
+    
+    return vedere;
+}
+
+void master()
+{
+    struct Pozitie pozitiiPosibile[10000];
+    int capatStiva = 0;
+    
+    struct Vedere vedereCurenta = getVedere();
+    
+    ///
+    // Gasim pozitiile initiale posibile
+    ///
+    for(int i = 1; i <= N; i++)
+        for(int j = 1; j <= M; j++)
+        {
+            //Directia N
+            if(vedereCurenta.fata == Harta[i-1][j] && vedereCurenta.dreapta == Harta[i][j+1] && vedereCurenta.spate == Harta[i+1][j] && vedereCurenta.stanga == Harta[i][j-1])
+            {
+                struct Pozitie pozitie;
+                pozitie.directie = 'N';
+                pozitie.i = i;
+                pozitie.j = j;
+                pozitie.iAnterior = -1;
+                pozitie.jAnterior = -1;
+                pozitiiPosibile[capatStiva++] = pozitie;
+            }
+            
+            //Directia S
+            if(vedereCurenta.fata == Harta[i+1][j] && vedereCurenta.dreapta == Harta[i][j-1] && vedereCurenta.spate == Harta[i-1][j] && vedereCurenta.stanga == Harta[i][j+1])
+            {
+                struct Pozitie pozitie;
+                pozitie.directie = 'S';
+                pozitie.i = i;
+                pozitie.j = j;
+                pozitie.iAnterior = -1;
+                pozitie.jAnterior = -1;
+                pozitiiPosibile[capatStiva++] = pozitie;
+            }
+            
+            //Directia E
+            if(vedereCurenta.fata == Harta[i][j+1] && vedereCurenta.dreapta == Harta[i+1][j] && vedereCurenta.spate == Harta[i][j-1] && vedereCurenta.stanga == Harta[i-1][j])
+            {
+                struct Pozitie pozitie;
+                pozitie.directie = 'E';
+                pozitie.i = i;
+                pozitie.j = j;
+                pozitie.iAnterior = -1;
+                pozitie.jAnterior = -1;
+                pozitiiPosibile[capatStiva++] = pozitie;
+            }
+            
+            //Directia V
+            if(vedereCurenta.fata == Harta[i][j-1] && vedereCurenta.dreapta == Harta[i-1][j] && vedereCurenta.spate == Harta[i][j+1] && vedereCurenta.stanga == Harta[i+1][j])
+            {
+                struct Pozitie pozitie;
+                pozitie.directie = 'V';
+                pozitie.i = i;
+                pozitie.j = j;
+                pozitie.iAnterior = -1;
+                pozitie.jAnterior = -1;
+                pozitiiPosibile[capatStiva++] = pozitie;
+            }
+
+        }
+}
+
+void slave()
+{
+    
+}
